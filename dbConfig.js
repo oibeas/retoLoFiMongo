0ee7f8b0b09608cc1f685a73c44b8ef0059fe860
oibeas <oscaribeas@gmail.com>
2020-11-19 13:51:34 +0100

    make it better

--- dbConfig.js
+++ dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const urlDb = 'mongodb://127.0.0.1/retoLoFi';
-// const urlDb = 'mongodb+srv://retolofi:amdk626277A@cluster0.npx3z.mongodb.net/retoLoFi?retryWrites=true&w=majority'
+// const urlDb = 'mongodb://127.0.0.1/retoLoFi';
+const urlDb = 'mongodb+srv://retolofi:amdk626277A@cluster0.npx3z.mongodb.net/retoLoFi?retryWrites=true&w=majority'
 
 mongoose.connect(urlDb, {
     useNewUrlParser: true,
